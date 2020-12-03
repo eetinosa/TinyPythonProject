0940d839b9c2584b175e5d336a0b121b0c082594
eetinosa <etinosa.enobun@gmail.com>
2020-12-03 07:29:04 +0100

    Added Chapter 5

---
+++ 05_Howler/howler.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""
+Author : etinosa <etinosa@localhost>
+Date   : 2020-12-01
+Purpose: Rock the Casbah
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Rock the Casbah',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('text',
+                        metavar='text',
+                        type=str,
+                        help='upper case input')
+
+    parser.add_argument('-o',
+                        '--outfile',
+                        help='output filename',
+                        metavar='str',
+                        type=str,
+                        default='')
+
+   
+
+    parser.add_argument('-f',
+                        '--file',
+                        help='A readable file',
+                        metavar='FILE',
+                        type=argparse.FileType('rt'),
+                        default=None)
+
+    args = parser.parse_args()
+
+    if os.path.isfile(args.text):
+        args.text = open(args.text).read().rstrip()
+
+    return args
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    out_fh = open(args.outfile, 'wt') if args.outfile else sys.stdout
+    out_fh.write(args.text.upper() + '\n')
+    out_fh.close()
+    
+
+ 
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
